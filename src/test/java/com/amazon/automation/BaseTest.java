89938f5684895c553c208b7b170b4525f76831a0
vigneshperumal2498 <perumalsamy.v@northeastern.edu>
2024-07-29 22:13:45 -0400

    Added chromedriver and completed the inital test case successfully

--- src/test/java/com/amazon/automation/BaseTest.java
+++ src/test/java/com/amazon/automation/BaseTest.java
@@ -3,17 +3,19 @@ package com.amazon.automation;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
         // Set the path to the chromedriver executable
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "src/test/java/com/amazon/automation/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
